4271637f99724f881c39c04bfad75e67680bf24f
Eduardo Maia <emaiax@fastmail.com>
2024-10-18 17:57:24 -0300

    feat: mimic common/builder to build lxc

--- builder/proxmox/lxc/builder.go
+++ builder/proxmox/lxc/builder.go
@@ -5,20 +5,28 @@ package proxmoxlxc
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
-	proxmoxapi "github.com/Telmate/proxmox-api-go/proxmox"
-	proxmoxcommon "github.com/hashicorp/packer-plugin-proxmox/builder/proxmox/common"
-	packersdk "github.com/hashicorp/packer-plugin-sdk/packer"
-
+	"github.com/Telmate/proxmox-api-go/proxmox"
 	"github.com/hashicorp/hcl/v2/hcldec"
+	common "github.com/hashicorp/packer-plugin-proxmox/builder/proxmox/common"
 	"github.com/hashicorp/packer-plugin-sdk/multistep"
+	"github.com/hashicorp/packer-plugin-sdk/multistep/commonsteps"
+	packersdk "github.com/hashicorp/packer-plugin-sdk/packer"
 )
 
 // The unique id for the builder
 const BuilderID = "proxmox.lxc"
 
 type Builder struct {
-	config Config
+	id            string
+	config        Config
+	coreSteps     []multistep.Step // override entire coreSteps
+	preSteps      []multistep.Step
+	postSteps     []multistep.Step
+	runner        multistep.Runner
+	proxmoxClient *proxmox.Client
 }
 
 // Builder implements packersdk.Builder
@@ -35,21 +43,68 @@ func (b *Builder) Prepare(raws ...interface{}) ([]string, []string, error) {
 func (b *Builder) Run(ctx context.Context, ui packersdk.Ui, hook packersdk.Hook) (packersdk.Artifact, error) {
 	state := new(multistep.BasicStateBag)
 
-	// state.Put("lxc-config", &b.config)
+	var err error
 
-	coreSteps := []multistep.Step{}
-	preSteps := []multistep.Step{}
-	postSteps := []multistep.Step{}
+	b.proxmoxClient, err = common.NewProxmoxClient(b.config.Config)
+	if err != nil {
+		return nil, err
+	}
 
-	state.Put("template_id", 983724) // placeholder for now
+	// Set up the state
+	state.Put("config", &b.config)
+	state.Put("proxmoxClient", b.proxmoxClient)
+	state.Put("hook", hook)
+	state.Put("ui", ui)
 
-	sb := proxmoxcommon.NewCustomSharedBuilder(BuilderID, b.config.Config, coreSteps, preSteps, postSteps, &lxcVMCreator{})
-	return sb.Run(ctx, ui, hook, state)
+	b.coreSteps = []multistep.Step{&stepCreateLxc{}}
+	b.preSteps = []multistep.Step{}
+	b.postSteps = []multistep.Step{}
+
+	steps := b.coreSteps
+	steps = append(b.preSteps, steps...)
+	steps = append(steps, b.postSteps...)
+
+	// placeholder to successfully run the builder
+	state.Put("template_id", 983724)
+	// sb := proxmoxcommon.NewCustomSharedBuilder(BuilderID, b.config.Config, coreSteps, preSteps, postSteps, &ProxmoxVMCreator{})
+	// return sb.Run(ctx, ui, hook, state)
+
+	// TODO
+	// hostComm := &b.config.Comm
+
+	// Run the steps
+	b.runner = commonsteps.NewRunner(steps, b.config.PackerConfig, ui)
+	b.runner.Run(ctx, state)
+
+	// If there was an error, return that
+	if rawErr, ok := state.GetOk("error"); ok {
+		return nil, rawErr.(error)
+	}
+
+	// If we were interrupted or cancelled, then just exit.
+	if _, ok := state.GetOk(multistep.StateCancelled); ok {
+		return nil, errors.New("build was cancelled")
+	}
+
+	// Verify that the template_id was set properly, otherwise we didn't progress through the last step
+	templateId, ok := state.Get("template_id").(int)
+	if !ok {
+		return nil, fmt.Errorf("template ID could not be determined")
+	}
+
+	artifact := common.NewArtifact(
+		b.id,
+		templateId,
+		b.proxmoxClient,
+		map[string]interface{}{"generated_data": state.Get("generated_data")},
+	)
+
+	return artifact, nil
 }
 
-type lxcVMCreator struct{}
+// type lxcVMCreator struct{}
 
-func (*lxcVMCreator) Create(vmRef *proxmoxapi.VmRef, config proxmoxapi.ConfigQemu, state multistep.StateBag) error {
-	client := state.Get("proxmoxClient").(*proxmoxapi.Client)
-	return config.Create(vmRef, client)
-}
+// func (*lxcVMCreator) Create(vmRef *proxmox.VmRef, config proxmox.ConfigLxc, state multistep.StateBag) error {
+// 	client := state.Get("proxmoxClient").(*proxmox.Client)
+// 	return config.CreateLxc(vmRef, client)
+// }
